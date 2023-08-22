d715add954b42993fdfc4c1ace10e636ee52e1ad
dxndre-onqor <dandre.phillips@onqor.co.uk>
2023-08-22 15:04:48 +0100

    Progress as of 22/08/2023 - Homepage foundations complete - About Us foundations complete - Get in Touch foundations complete - Our Work currently in progress

---
+++ app/public/wp-content/themes/graphic-point-full-site/blocks/contact.php
@@ -0,0 +1,35 @@
+<!-- Get in Touch Section -->
+
+<section class="get-in-touch">
+
+    <?php
+        $contactHeader = get_sub_field('contact_header');
+        $leadingText = get_sub_field('leading_text');
+        $openingHours = get_sub_field('opening_hours');
+
+        $phoneNumber = get_field('phone_number', 'option');
+        $phoneIcon = get_sub_field('phone_icon');
+
+        $emailAddress = get_field('email_address', 'option');
+        $emailIcon = get_sub_field('email_icon');
+    ?>
+
+    <div class="container">
+
+        <div class="row">
+            <h2><?php echo $contactHeader ?></h2>
+            <p class="lead"><?php echo $leadingText ?></p>
+            <hr>
+            <p class="lead"><?php echo $openingHours ?></p>
+        </div>
+        <div class="row contact-stats">
+            <div class="col-6">
+                <a href="tel:<?php echo $phoneNumber ?>"><img src="<?php echo $phoneIcon['url'];?>" alt="<?php echo $phoneIcon['alt'];?>"> <?php echo $phoneNumber ?></a>
+            </div>
+            <div class="col-6">
+                <a href="mailto:<?php echo $emailAddress ?>"><img src="<?php echo $emailIcon['url'];?>" alt="<?php echo $emailIcon['alt'];?>"> <?php echo $emailAddress ?></p></a>
+            </div>
+        </div>
+
+    </div>
+</section>
